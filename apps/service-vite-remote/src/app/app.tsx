2af74178615e0cd775ca73a4b4f32db770164c8c
amaralc <amaral.calil@gmail.com>
2023-04-06 07:34:59 -0300

    feat: add button to remote

--- apps/service-vite-remote/src/app/app.tsx
+++ apps/service-vite-remote/src/app/app.tsx
@@ -1,13 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-
+import Button from '../components/button';
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <>
+      <h1>Remote Application</h1>
+      <Button />
       <NxWelcome title="service-vite-remote" />
-
       <div />
     </>
   );
