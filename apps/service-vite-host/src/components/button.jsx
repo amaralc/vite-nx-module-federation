63e0ca03eb4dc026aba13efddbea24148a3b2f6b
amaralc <amaral.calil@gmail.com>
2023-04-07 16:26:40 -0300

    feat: differentiate buttons styles

--- apps/service-vite-host/src/components/button.jsx
+++ apps/service-vite-host/src/components/button.jsx
@@ -6,7 +6,7 @@ export const Button = () => {
   const [state, setState] = useState(0);
   return (
     <div>
-      <button className="shared-btn" onClick={() => setState((s) => s + 1)}>
+      <button className="host-btn" onClick={() => setState((s) => s + 1)}>
         (host) Counter: {state}
       </button>
     </div>
